d174af29efc6a52f08bd290a84e4eaed89a76b86
Facundo Ponce <facuponce619@gmail.com>
2023-06-22 22:43:01 -0300

    converterList, convert

--- src/app/converter-display/converter-display.component.ts
+++ src/app/converter-display/converter-display.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CurrencyService } from './currency.service';
 
 @Component({
   selector: 'app-converter-display',
   templateUrl: './converter-display.component.html',
   styleUrls: ['./converter-display.component.scss']
 })
-export class ConverterDisplayComponent {
+export class ConverterDisplayComponent implements OnInit {
+  currencyArray:any;
+  from='';
+  to='';
+  fromCurrency: any;
+  toCurrency: any;
+
+  constructor(private currencyService:CurrencyService) {
+  }
+
+  ngOnInit(){
+    this.currencyService.getCurrencyData().subscribe(list=>{
+      this.currencyArray = this.convertObjectToArray(list.currencies);
+      console.log(this.currencyArray);
+    })
+  }
+
+  private convertObjectToArray(obj: { [key: string]: string }): { code: string, name: string }[] {
+    return Object.keys(obj).map(key => ({ code: key, name: obj[key] }));
+  }
+
+  convertCurrency(from: string, to: string, fromCurrency: any, toCurrency: any){
+    //console.log(from, to);
+    //console.log(fromCurrency, toCurrency);
+    //TODO:callConvertService
+  };
+  onSelectCurrency(event: any){
+    //console.log(event);
+    //TODO:updateDropDownSelection
+  };
 
 }
---
+++ src/app/converter-display/currency.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CurrencyService {
+
+  private readonly API_URL = 'https://api.apilayer.com/currency_data/list';
+  private readonly CONVERT_API_URL = 'https://api.apilayer.com/currency_data/convert';
+  private readonly API_KEY = 'J4yXuMtVYHkB9kKQrK22OFbaBuQ8FasR';
+
+  constructor(private http: HttpClient) { }
+
+  getCurrencyData(): Observable<any> {
+    const headers = new HttpHeaders().append('apikey', this.API_KEY);
+    const requestOptions = {
+      headers: headers
+    };
+
+    return this.http.get(this.API_URL, requestOptions);
+  }
+
+  convertCurrency(to: string, from: string, amount: number): Observable<any> {
+    const url = `${this.CONVERT_API_URL}?to=${to}&from=${from}&amount=${amount}`;
+    const headers = new HttpHeaders().append('apikey', this.API_KEY);
+    const requestOptions = {
+      headers: headers
+    };
+
+    return this.http.get(url, requestOptions);
+  }
+
+  }
+
+//list
+//live
+//convert
+//historical
+//timeframe
+//change
+
